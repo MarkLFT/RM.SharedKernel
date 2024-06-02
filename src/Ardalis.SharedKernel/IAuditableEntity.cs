51d6a6e0948f83ab059b91f939e5ac3fff6e5ac0
Mark Townsend <mark@resort-manager.com>
2024-06-02 18:45:18 +0800

    chore: Added ISoftDelete and IAuditableEntity flags to allow managing CreatedOn, DeletedOn, UpdatedOn.

---
+++ src/Ardalis.SharedKernel/IAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Ardalis.SharedKernel;
+
+/// <summary>
+/// Apply this marker interface to entities that should be audited
+/// </summary>
+public interface IAuditableEntity
+{
+  public DateTime CreatedOn { get; set; }
+  public DateTime LastModifiedOn { get; set; }
+}
