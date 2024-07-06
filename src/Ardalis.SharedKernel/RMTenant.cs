979d2c8d86b26151af08d28e79cfdba45a867f50
Mark Townsend <mark@resort-manager.com>
2024-07-06 09:23:40 +0800

    chore: Added RMTenant class

---
+++ src/Ardalis.SharedKernel/RMTenant.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Finbuckle.MultiTenant.Abstractions;
+
+namespace rm.micro.tenants.Core.TenantAggregate;
+public class RMTenant : ITenantInfo
+{
+  public RMTenant() { }
+
+  public RMTenant([NotNull] string id, [NotNull] string identifier, [NotNull] string name, string? connectionString)
+  {
+    Id = id;
+    Identifier = identifier;
+    Name = name;
+    ConnectionString = connectionString;
+  }
+
+  public string? Id { get; set; }
+  public string? Identifier { get; set; }
+  public string? Name { get; set; }
+  public string? ConnectionString { get; set; }
+
+}
