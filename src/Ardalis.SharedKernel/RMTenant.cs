d3204c363a71384bc562d7f2167deb0dccdf7b37
Mark Townsend <mark@resort-manager.com>
2024-07-10 15:06:29 +0800

    chore: Changed finbuckle to 6.13.1 to allow following tutorial.

--- src/Ardalis.SharedKernel/RMTenant.cs
+++ src/Ardalis.SharedKernel/RMTenant.cs
@@ -1,22 +1,27 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using Finbuckle.MultiTenant.Abstractions;
+using Finbuckle.MultiTenant;
 
 namespace RM.SharedKernel;
 public class RMTenant : ITenantInfo
 {
   public RMTenant() { }
 
-  public RMTenant([NotNull] string id, [NotNull] string identifier, [NotNull] string name, string? connectionString)
+  public RMTenant([NotNull] string id, [NotNull] string identifier, [NotNull] string name, string? connectionString, string adminEmail, DateTime validUpTo)
   {
     Id = id;
     Identifier = identifier;
     Name = name;
     ConnectionString = connectionString;
+    AdminEmail = adminEmail;
+    ValidUpTo = validUpTo;
   }
 
   public string? Id { get; set; }
   public string? Identifier { get; set; }
   public string? Name { get; set; }
   public string? ConnectionString { get; set; }
+  public string? AdminEmail { get; set; }
+  public DateTime ValidUpTo { get; set; }
+  public bool IsActive { get; set; }
 
 }
